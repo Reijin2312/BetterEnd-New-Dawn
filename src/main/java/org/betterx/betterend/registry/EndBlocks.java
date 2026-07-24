d5426db0e7850d4df76c61d8cc967ce4656523f5
Raijin <the_krap7@mail.ru>
2026-07-25 01:32:57 +0500

    Let umbrella moss and jungle grass grow on common soil
    Ports the final behavior from:
    
    https://github.com/quiqueck/BetterEnd/commit/c572ce07

--- src/main/java/org/betterx/betterend/registry/EndBlocks.java
+++ src/main/java/org/betterx/betterend/registry/EndBlocks.java
@@ -452,7 +452,7 @@ public class EndBlocks {
         AMBER_GRASS = registerBlock( "amber_grass", new TerrainPlantBlock(AMBER_MOSS) );
         TWISTED_UMBRELLA_MOSS = registerBlock( "twisted_umbrella_moss", new TwistedUmbrellaMossBlock() );
         TWISTED_UMBRELLA_MOSS_TALL = registerBlock( "twisted_umbrella_moss_tall", new TwistedUmbrellaMossTallBlock() );
-        JUNGLE_GRASS = registerBlock( "jungle_grass", new TerrainPlantBlock(JUNGLE_MOSS) );
+        JUNGLE_GRASS = registerBlock( "jungle_grass", new TerrainPlantBlock(CommonBlockTags.SOIL) );
         BLOOMING_COOKSONIA = registerBlock( "blooming_cooksonia", new TerrainPlantBlock(END_MOSS) );
         SALTEAGO = registerBlock("salteago", new TerrainPlantBlock(END_MOSS));
         VAIOLUSH_FERN = registerBlock("vaiolush_fern", new TerrainPlantBlock(END_MOSS));
