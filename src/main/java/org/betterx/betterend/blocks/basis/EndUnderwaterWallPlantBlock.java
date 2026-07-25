861a25a399412967571a60d02dbb685f54428694
Raijin <the_krap7@mail.ru>
2026-07-25 18:24:22 +0500

    Allow End decorations on solid supports
    Ports upstream BetterEnd c4618e06 and c71955d9 to the legacy block hierarchy. Wall plants and hanging seeds now accept sturdy faces and leaves without advertising obsolete End Stone-only survival rules.

--- src/main/java/org/betterx/betterend/blocks/basis/EndUnderwaterWallPlantBlock.java
+++ src/main/java/org/betterx/betterend/blocks/basis/EndUnderwaterWallPlantBlock.java
@@ -2,12 +2,11 @@ package org.betterx.betterend.blocks.basis;
 
 import org.betterx.bclib.behaviours.BehaviourBuilders;
 import org.betterx.bclib.blocks.BaseUnderwaterWallPlantBlock;
-import org.betterx.betterend.interfaces.survives.SurvivesOnEndStone;
 
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.MapColor;
 
-public class EndUnderwaterWallPlantBlock extends BaseUnderwaterWallPlantBlock implements SurvivesOnEndStone {
+public class EndUnderwaterWallPlantBlock extends BaseUnderwaterWallPlantBlock {
 
     public EndUnderwaterWallPlantBlock(MapColor color) {
         super(BehaviourBuilders.createWaterPlant(color));
@@ -15,6 +14,6 @@ public class EndUnderwaterWallPlantBlock extends BaseUnderwaterWallPlantBlock im
 
     @Override
     public boolean isTerrain(BlockState state) {
-        return SurvivesOnEndStone.super.isTerrain(state);
+        return true;
     }
 }
